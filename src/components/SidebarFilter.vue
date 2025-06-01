37c33327886054852cd4aac9dc49df315b4688fd
Zhuowen Li <ansonli4537577@gmail.com>
2025-05-31 21:45:46 -0700

    debugging sidebarfilter

--- src/components/SidebarFilter.vue
+++ src/components/SidebarFilter.vue
@@ -34,7 +34,8 @@ const fetchCategories = async () => {
   try {
     const res = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/api/categories`)
     console.log("Fetched categories:", res.data)
-    categories.value = res.data.map((cat: { name: string }) => cat.name)
+    categories.value = res.data.categories.map((cat: { name: string }) => cat.name)
+
   } catch (err) {
     console.error('Failed to fetch categories:', err)
   }
