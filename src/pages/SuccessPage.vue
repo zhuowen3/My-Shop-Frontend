4d8d4c5c4698f5bd931dfa66be53c771106ad3da
Zhuowen Li <ansonli4537577@gmail.com>
2025-06-04 00:21:27 -0700

    debugging sidebarfilter

--- src/pages/SuccessPage.vue
+++ src/pages/SuccessPage.vue
@@ -4,3 +4,11 @@
     <p>Your order has been placed. Thank you!</p>
   </div>
 </template>
+<script lang="ts">
+import { useCartStore } from '@/stores/cart'
+import { onMounted } from 'vue'
+onMounted(() => {
+  const cart = useCartStore()
+  cart.clear()  // or cart.items = []
+})
+</script>
