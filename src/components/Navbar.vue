bdb28b52be7b4942c417621a67a810d518c1f321
Zhuowen Li <ansonli4537577@gmail.com>
2025-08-09 19:09:54 -0700

    Implementing new features

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -91,7 +91,7 @@ const router = useRouter()
 const route = useRoute()
 
 // Set your logo path if you have one
-const logoSrc = '/logo.svg' // e.g. new URL('@/assets/logo.png', import.meta.url).href
+const logoSrc = '/logo_transparent3.png' // e.g. new URL('@/assets/logo.png', import.meta.url).href
 
 // Search
 const search = ref('')
